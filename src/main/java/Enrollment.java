ee3c9f0a5e4e758d9bf9f82b9d9b7a1d56a09e28
Luciel <55259900+vLuciel@users.noreply.github.com>
2021-04-11 22:40:31 -0600

    1.1.5
    - Updated a bit of the sql

---
+++ src/main/java/Enrollment.java
@@ -0,0 +1,11 @@
+/*
+ * CS3810 - Principles of Database Systems - Spring 2021
+ * Instructor: Thyago Mota
+ * Description: DB 03 - Enrollment
+ * Student(s) Name(s): Brandon Young, Stuart Griffin, Timothy Trusov
+ */
+
+public class Enrollment {
+
+
+}
---
+++ src/main/java/EnrollmentPK.java
@@ -0,0 +1,13 @@
+/*
+ * CS3810 - Principles of Database Systems - Spring 2021
+ * Instructor: Thyago Mota
+ * Description: DB 03 - EnrollmentPK
+ * Student(s) Name(s): Brandon Young, Stuart Griffin, Timothy Trusov
+ */
+
+import java.io.Serializable;
+
+public class EnrollmentPK implements Serializable {
+
+
+}
