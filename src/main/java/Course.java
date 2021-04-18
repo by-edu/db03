34754b143108333bf48bc43c6b1ca86ef1aacfb7
Luciel <55259900+vLuciel@users.noreply.github.com>
2021-04-17 22:19:19 -0600

    1.2.0
    + Made them, not entirely sure if this is it, but I'm sure this is about correct.

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -4,8 +4,49 @@
  * Description: DB 03 - Student
  * Student(s) Name(s): Brandon Young, Stuart Griffin, Timothy Trusov
  */
+import javax.persistence.*;
 
+@Entity
+@Table(name = "courses")
 public class Course {
 
+
+        private String code;
+
+        private String title;
+
+        private String instructor;
+
+        private int max;
+
+        private int actual;
+
+        public String getCode(){ return code; }
+
+        public void setCode(String code){ this.code = code; }
+
+        public String getTitle(){ return title; }
+
+        public void setTitle(String title){ this.title = title; }
+
+        public String getInstructor(){ return instructor; }
+
+        public void setInstructor(String instructor){ this.instructor = instructor; }
+
+        public int getMax(){ return max; }
+
+        public void setMax(int max){ this.max = max; }
+
+        public int getActual(){ return actual; }
+
+        public void setActual(int actual){ this.actual = actual; }
+
+
+        @Override
+        public String toString() {return code + "-"
+                + title + "-"
+                + instructor + "-"
+                + max + "-"
+                + actual;}
 
 }
--- src/main/java/Enrollment.java
+++ src/main/java/Enrollment.java
@@ -4,8 +4,31 @@
  * Description: DB 03 - Enrollment
  * Student(s) Name(s): Brandon Young, Stuart Griffin, Timothy Trusov
  */
+import javax.persistence.*;
 
+@Entity
+@Table(name = "enrollments")
 public class Enrollment {
+
+    private String code;
+
+    @Id
+    private int id;
+
+    public String getCode(){return code;}
+
+    public void setCode(String code){this.code = code;}
+
+    public int getId(){return id;}
+
+    public void setId(int id){this.id = id;}
+
+   @Override
+    public String toString() {return code + "-" + id;}
+
+    // @OneToMany for the primary key thing??
+
+
 
 
 }
